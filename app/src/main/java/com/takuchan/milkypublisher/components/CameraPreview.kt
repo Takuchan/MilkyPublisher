bc9009a8a0530a21861f9520ca2eb739c87aabef
Takuchan <takuchanapp@gmail.com>
2023-12-11 17:31:56 +0900

    Revert "画面に処理後の画像を出す準備"
    This reverts commit 7f8dc9d9901965fd6a74df8445bbccbbaf8fecfc.

--- app/src/main/java/com/takuchan/milkypublisher/components/CameraPreview.kt
+++ app/src/main/java/com/takuchan/milkypublisher/components/CameraPreview.kt
@@ -18,7 +18,6 @@ import com.google.android.gms.tasks.Task
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.pose.Pose
 import com.google.mlkit.vision.pose.PoseDetection
-import com.google.mlkit.vision.pose.PoseLandmark
 import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
 import com.takuchan.milkypublisher.analysis.CaptureImageAnalyzer
 import com.takuchan.milkypublisher.background.getCameraProvider
@@ -60,14 +59,8 @@ fun CameraPreview(
                         frameImage.image?.let { it1 -> InputImage.fromMediaImage(it1,frameImage.imageInfo.rotationDegrees) }
                     GlobalScope.launch(Dispatchers.IO) {
                         val result = poseDetect.process(image!!)
-                            .addOnSuccessListener { pose ->
-                                Log.d("left_sholder",
-                                    pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER).toString()
-                                )
-                                Log.d("PoseDetect","成功しました")
-                            }
+                            .addOnSuccessListener { Log.d("PoseDetect","成功しました") }
                             .addOnCanceledListener { Log.d("PoseDetect","キャンセルされました") }
-
                     }
                 })
             }
