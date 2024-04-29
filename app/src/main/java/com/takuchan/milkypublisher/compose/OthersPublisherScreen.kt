ab0063b938f9b77d6df9c26b8bd8e4b822c24f88
Takuchan <takuchanapp@gmail.com>
2024-04-29 23:50:29 +0900

    センサーの画面を表示できるようになった

--- app/src/main/java/com/takuchan/milkypublisher/compose/OthersPublisherScreen.kt
+++ app/src/main/java/com/takuchan/milkypublisher/compose/OthersPublisherScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -47,102 +48,143 @@ fun OthersPublisherScreen(
     ) {
         Text(text = "トピックで送信するデータを選択してください")
 //        AccelerometerSample()
-        GridExample()
+        SensorScreen()
 
     }
 }
 
 @Composable
-fun GridExample() {
+fun SensorScreen() {
+    val context = LocalContext.current
+    val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+    val gyroscopeValue = remember { mutableStateOf("") }
+    val pressureValue = remember { mutableStateOf("") }
+    val temperatureValue = remember { mutableStateOf("") }
+    val lightValue = remember { mutableStateOf("") }
+
+    // Gyroscope sensor listener
+    val gyroscopeListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+        override fun onSensorChanged(event: SensorEvent?) {
+            event?.let {
+                val value = "X: ${it.values[0]}, Y: ${it.values[1]}, Z: ${it.values[2]}"
+                gyroscopeValue.value = value
+            }
+        }
+    }
+
+    // Pressure sensor listener
+    val pressureListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+        override fun onSensorChanged(event: SensorEvent?) {
+            event?.let {
+                val value = "${it.values[0]} hPa"
+                pressureValue.value = value
+            }
+        }
+    }
+
+    // Temperature sensor listener
+    val temperatureListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+        override fun onSensorChanged(event: SensorEvent?) {
+            event?.let {
+                val value = "${it.values[0]} °C"
+                temperatureValue.value = value
+            }
+        }
+    }
+
+    // Light sensor listener
+    val lightListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+        override fun onSensorChanged(event: SensorEvent?) {
+            event?.let {
+                val value = "${it.values[0]} lx"
+                lightValue.value = value
+            }
+        }
+    }
+
+    // Register sensor listeners
+    DisposableEffect(Unit) {
+        val gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        val pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
+        val temperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+        val light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+        sensorManager.registerListener(gyroscopeListener, gyroscope, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(pressureListener, pressure, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(temperatureListener, temperature, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(lightListener, light, SensorManager.SENSOR_DELAY_NORMAL)
+
+        onDispose {
+            sensorManager.unregisterListener(gyroscopeListener)
+            sensorManager.unregisterListener(pressureListener)
+            sensorManager.unregisterListener(temperatureListener)
+            sensorManager.unregisterListener(lightListener)
+        }
+    }
+
+    Column {
+        Text(
+            text = "Sensor Data",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(16.dp)
+        )
+        SensorGrid(
+            gyroscopeValue = gyroscopeValue.value,
+            pressureValue = pressureValue.value,
+            temperatureValue = temperatureValue.value,
+            lightValue = lightValue.value
+        )
+    }
+}
+@Composable
+fun SensorGrid(
+    gyroscopeValue: String,
+    pressureValue: String,
+    temperatureValue: String,
+    lightValue: String
+) {
+    val sensorData = listOf(
+        Pair("Gyroscope", gyroscopeValue),
+        Pair("Pressure", pressureValue),
+        Pair("Temperature", temperatureValue),
+        Pair("Light", lightValue)
+    )
+
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        items(9) { index ->
-            Box(
-                modifier = Modifier
-                    .aspectRatio(1f)
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(Color.LightGray),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "Item $index",
-                    style = MaterialTheme.typography.titleLarge,
-                    color = Color.Black
-                )
-            }
+        items(sensorData) { (sensorName, sensorValue) ->
+            SensorItem(sensorName, sensorValue)
         }
     }
 }
 
 @Composable
-fun CardButton(
-    floatX: Float,
-    floatY: Float,
-    floatZ: Float,
-    title: String,
-    onClick: () -> Unit
-) {
-    var isPressed by remember { mutableStateOf(false) }
-
+fun SensorItem(sensorName: String, sensorValue: String) {
     Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .clickable {
-                isPressed = !isPressed
-                onClick()
-            },
-        shape = RoundedCornerShape(16.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = if (isPressed) MaterialTheme.colorScheme.primaryContainer else MaterialTheme.colorScheme.surfaceVariant
-        )
+        modifier = Modifier.fillMaxWidth()
     ) {
         Column(
-            modifier = Modifier.padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            modifier = Modifier.padding(16.dp)
         ) {
-            Text(text = title, style = MaterialTheme.typography.titleLarge)
-            Text(text = "X: $floatX")
-            Text(text = "Y: $floatY")
-            Text(text = "Z: $floatZ")
+            Text(
+                text = sensorName,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = sensorValue,
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
 
-
-@Composable
-fun AccelerometerSample() {
-    val context = LocalContext.current
-    val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-
-    var acceleration by remember { mutableStateOf(Triple(0f, 0f, 0f)) }
-
-    DisposableEffect(Unit) {
-        val eventListener = object : SensorEventListener {
-            override fun onSensorChanged(event: SensorEvent) {
-                acceleration = Triple(event.values[0], event.values[1], event.values[2])
-            }
-
-            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
-        }
-
-        sensorManager.registerListener(
-            eventListener,
-            accelerometer,
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
-
-        onDispose {
-            sensorManager.unregisterListener(eventListener)
-        }
-    }
-
-    CardButton(acceleration.first, acceleration.second, acceleration.third, "Accelerometer Data", {})
-
-}
