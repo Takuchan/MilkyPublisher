4c77fa9cd8789cfc91307afaf92949a448b7672a
Takuchan <takuchanapp@gmail.com>
2024-09-12 01:57:40 +0900

    ViewModelを変更して新しい値が入ってきたらそのインスタンスの中の一部が変更されるようになった

--- app/src/main/java/com/takuchan/milkypublisher/ViewModel/publisherscreen/PublisherScreenViewModel.kt
+++ app/src/main/java/com/takuchan/milkypublisher/ViewModel/publisherscreen/PublisherScreenViewModel.kt
@@ -6,24 +6,52 @@ import com.takuchan.milkypublisher.data.model.publisherscreen.PublisherState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
-
 
 @HiltViewModel
 class PublisherScreenViewModel @Inject constructor(): ViewModel(){
-    private val _uiState = MutableStateFlow<PublisherState>(PublisherState())
-    val uiState: StateFlow<PublisherState> = _uiState
+    private val _uiState = MutableStateFlow(PublisherState())
+    val uiState: StateFlow<PublisherState> = _uiState.asStateFlow()
 
     fun setPublishState(state: Boolean){
-        _uiState.value.publisherDetectionState.isPublisher = state
+        _uiState.update { currentState ->
+            currentState.copy(
+                publisherDetectionState = currentState.publisherDetectionState.copy(
+                    isPublisher = state
+                )
+            )
+        }
     }
+
     fun setPoseDetection(state: Boolean){
-        _uiState.value.publisherDetectionState.isDetectPose = state
+        _uiState.update { currentState ->
+            currentState.copy(
+                publisherDetectionState = currentState.publisherDetectionState.copy(
+                    isDetectPose = state
+                )
+            )
+        }
     }
+
     fun setFaceDetection(state: Boolean){
-        _uiState.value.publisherDetectionState.isDetectFace = state
+        _uiState.update { currentState ->
+            currentState.copy(
+                publisherDetectionState = currentState.publisherDetectionState.copy(
+                    isDetectFace = state
+                )
+            )
+        }
     }
+
     fun setFaceDetectionSmiling(state: Boolean){
-        _uiState.value.publisherDetectionState.isDetectFaceSmiling = state
+        _uiState.update { currentState ->
+            currentState.copy(
+                publisherDetectionState = currentState.publisherDetectionState.copy(
+                    isDetectFaceSmiling = state
+                )
+            )
+        }
     }
 }
